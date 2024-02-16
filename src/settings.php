24643e755523812e7db6ba42f3740ea838f91927
Luan Nogueira <luannogueira093@gmail.com>
2024-02-16 14:39:47 -0300

    versão 3.0

---
+++ src/settings.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        'db' => [
+
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'slim',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => ''
+        ]
+    ],
+];
